cb6d3b6fce4cdeba6158078bf5d3d898b1f4f5eb
Namito Yokota <nyokota@crimson.ua.edu>
2022-12-02 08:40:53 -0600

    get location from browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projectjeep';
+export class AppComponent implements OnInit {
+    title = 'projectjeep';
+
+    /** On init lifecycle hook */
+    ngOnInit() {
+        this.getLocation();
+    }
+
+    /** Gets location from browser */
+    private getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition, this.handleError);
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    }
+
+    /** Prints out position in console */
+    private showPosition(position: any) {
+        console.log(
+            `Latitude: ${position.coords.latitude}, longitude: ${position.coords.longitude}`
+        );
+    }
+
+    /** Handles error from HTML Geolocation API */
+    private handleError(error: any) {
+        let errorStr;
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                errorStr = 'User denied the request for Geolocation.';
+                break;
+            case error.POSITION_UNAVAILABLE:
+                errorStr = 'Location information is unavailable.';
+                break;
+            case error.TIMEOUT:
+                errorStr = 'The request to get user location timed out.';
+                break;
+            case error.UNKNOWN_ERROR:
+                errorStr = 'An unknown error occurred.';
+                break;
+            default:
+                errorStr = 'An unknown error occurred.';
+        }
+        console.error('Error occurred: ' + errorStr);
+    }
 }
